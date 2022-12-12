0d10865f5d0dd9c42ba99c571b5fd978ebcf2743
JJ Style <style.jj@pm.me>
2022-12-12 19:46:06 +0000

    neaten up a bit and add argparsing

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
 #!/bin/python
 import subprocess
-from datetime import datetime
 import re
-import sys
 from pathlib import Path
-import dateutil.parser 
+import dateutil.parser
+import argparse
 
-date_re1 = re.compile(r"^.*(20[0-9]{2}[-_]?[0-9]{2}[-_]?[0-9]{2}[-_]?([0-9]{6}|[0-9]{2}[-_][0-9]{2}[-_][0-9]{2})).*$")
-whatsapp = re.compile(r"^.*(20\d{6})-WA.*$")
+date_regex = re.compile(r"^.*(20[0-9]{2}[-_]?[0-9]{2}[-_]?[0-9]{2}[-_]?([0-9]{6}|[0-9]{2}[-_][0-9]{2}[-_][0-9]{2})).*$")
+whatsapp_regex = re.compile(r"^.*(20\d{6})-WA.*$")
 
-path = Path(sys.argv[1])
-for file in path.rglob("*.jpeg"):
-    date = None
-    if match := date_re1.search(file.name):
-        date = match.groups()[0]
-    elif match := whatsapp.search(file.name):
-        date = match.groups()[0]
-    else:
-        pass
-    try:
-        date = re.sub("_", "-",date)
-        date = dateutil.parser.parse(date)
-        command = f'exiftool -overwrite_original -AllDates="{date}" "{file.absolute()}"'
-        #subprocess.run(command, shell=True, check=True)
-        print(command)
-    except:
-        pass
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--dry-run", action="store_true", default=False, help="show what would be executed. Nothing will be changed")
+    parser.add_argument("directory", type=str)
+
+    args = parser.parse_args()
+
+    path = Path(args.directory)
+
+    if not path.exists():
+        print(f"Directory '{args.directory}' does not exist")
+        exit(1)
+
+    for file in path.rglob("*.jpeg"):
+        date = None
+        if match := date_regex.search(file.name):
+            date = match.groups()[0]
+        elif match := whatsapp_regex.search(file.name):
+            date = match.groups()[0]
+
+        try:
+            date = re.sub("_", "-", date)
+            date = dateutil.parser.parse(date)
+            command = f'exiftool -overwrite_original -AllDates="{date}" "{file.absolute()}"'
+            if args.dry_run:
+                print(command)
+            else:
+                subprocess.run(command, shell=True, check=True)
+        except Exception as ex:
+            print(f"parsing date from '{file}': {ex}")
+            pass
+
+if __name__ == "__main__":
+    main()
