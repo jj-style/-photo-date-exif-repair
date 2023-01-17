e88f804974b8e85ab9e0153028f6e33fa45fa7ee
JJ Style <style.jj@pm.me>
2023-01-17 19:16:31 +0000

    refactor integration test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,43 +1,44 @@
 use chrono::TimeZone;
 use image::{ImageFormat, RgbImage};
 use photo_date_exif_repair::{run, Args};
-use std::fs::File;
-use tempfile::tempdir;
+use std::{fs::File, path::PathBuf};
+use tempfile::{tempdir, TempDir};
+
+struct TestData(PathBuf, TempDir);
 
 #[test]
 fn test_date_rename_e2e() -> Result<(), Box<dyn std::error::Error>> {
     // Arrange
-    let dir = tempdir()?;
-    let file_path = dir.path().join("IMG_20210820_133000_image.jpg");
-    let file = File::create(&file_path)?;
-
-    let img = RgbImage::new(32, 32);
-    img.save_with_format(&file_path, ImageFormat::Jpeg)?;
-
-    // Assert
-    //get_date_from_exif_from_file(&file)?;
-
+    let data = create_jpeg_with_filename("IMG_20210820_133000_image.jpg")?;
+    
     // Act
     let result = run(Args {
-        files: vec![file_path.to_str().unwrap().to_string()],
+        files: vec![data.0.to_str().unwrap().to_string()],
         dryrun: false,
         overwrite: true,
     });
 
     // Assert
     assert!(result.is_ok());
-    let got_datetime = get_date_from_exif_from_file(File::open(&file_path)?)?;
+    let got_datetime = get_date_from_exif_from_file(File::open(&data.0)?)?;
     let expected_datetime = chrono::Local
         .with_ymd_and_hms(2021, 8, 20, 13, 30, 0)
         .unwrap();
     assert_eq!(got_datetime, expected_datetime);
 
-    drop(file);
-    dir.close()?;
+    data.1.close()?;
     Ok(())
 }
 
-//fn create_jpeg_with_filename()
+fn create_jpeg_with_filename(filename: &str) -> Result<TestData, Box<dyn std::error::Error>> {
+    let dir = tempdir()?;
+    let file_path = dir.path().join(filename);
+    let _ = File::create(&file_path)?;
+
+    let img = RgbImage::new(32, 32);
+    img.save_with_format(&file_path, ImageFormat::Jpeg)?;
+    Ok(TestData(file_path, dir))
+}
 
 fn get_date_from_exif_from_file(
     file: File,
